098fc3fec80086f67a393963cb5455faf2524b0a
Bernerd Schaefer <bj.schaefer@gmail.com>
2010-09-11 15:11:37 -0500

    Document Capybara::Driver::Akephalos::Node

--- lib/akephalos/capybara.rb
+++ lib/akephalos/capybara.rb
@@ -1,7 +1,17 @@
+# Driver class exposed to Capybara. It implements Capybara's full driver API,
+# and is the entry point for interaction between the test suites and HtmlUnit.
+#
+# This class and +Capybara::Driver::Akephalos::Node+ are written to run on both
+# MRI and JRuby, and is agnostic whether the Akephalos::Client instance is used
+# directly or over DRb.
 class Capybara::Driver::Akephalos < Capybara::Driver::Base
 
+  # Akephalos-specific implementation for Capybara's Node class.
   class Node < Capybara::Node
 
+    # @api capybara
+    # @param [String] name attribute name
+    # @return [String] the attribute value
     def [](name)
       name = name.to_s
       case name
@@ -12,14 +22,22 @@ class Capybara::Driver::Akephalos < Capybara::Driver::Base
       end
     end
 
+    # @api capybara
+    # @return [String] the inner text of the node
     def text
       node.text
     end
 
+    # @api capybara
+    # @return [String] the form element's value
     def value
       node.value
     end
 
+    # Set the form element's value.
+    #
+    # @api capybara
+    # @param [String] value the form element's new value
     def set(value)
       if tag_name == 'textarea'
         node.value = value.to_s
@@ -34,6 +52,10 @@ class Capybara::Driver::Akephalos < Capybara::Driver::Base
       end
     end
 
+    # Select an option from a select box.
+    #
+    # @api capybara
+    # @param [String] option the option to select
     def select(option)
       result = node.select_option(option)
 
@@ -45,6 +67,10 @@ class Capybara::Driver::Akephalos < Capybara::Driver::Base
       end
     end
 
+    # Unselect an option from a select box.
+    #
+    # @api capybara
+    # @param [String] option the option to unselect
     def unselect(option)
       unless self[:multiple]
         raise Capybara::UnselectNotAllowed, "Cannot unselect option '#{option}' from single select box."
@@ -60,36 +86,54 @@ class Capybara::Driver::Akephalos < Capybara::Driver::Base
       end
     end
 
+    # Trigger an event on the element.
+    #
+    # @api capybara
+    # @param [String] event the event to trigger
     def trigger(event)
       node.fire_event(event.to_s)
     end
 
+    # @api capybara
+    # @return [String] the element's tag name
     def tag_name
       node.tag_name
     end
 
+    # @api capybara
+    # @return [true, false] the element's visiblity
     def visible?
       node.visible?
     end
 
+    # Drag the element on top of the target element.
+    #
+    # @api capybara
+    # @param [Node] element the target element
     def drag_to(element)
       trigger('mousedown')
       element.trigger('mousemove')
       element.trigger('mouseup')
     end
 
+    # Click the element.
     def click
       node.click
     end
 
     private
 
+    # Return all child nodes which match the selector criteria.
+    #
+    # @api capybara
+    # @return [Array<Node>] the matched nodes
     def all_unfiltered(selector)
       nodes = []
       node.find(selector).each { |node| nodes << Node.new(driver, node) }
       nodes
     end
 
+    # @return [String] the node's type attribute
     def type
       node[:type]
     end
