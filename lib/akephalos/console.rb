3027c42ef16f9d1d882f9c8323feb572680fd044
Bernerd Schaefer <bj.schaefer@gmail.com>
2010-09-11 14:57:59 -0500

    Document Akephalos::Console

--- lib/akephalos/console.rb
+++ lib/akephalos/console.rb
@@ -1,3 +1,5 @@
+# Begin a new Capybara session, by default connecting to localhost on port
+# 3000.
 def session
   Capybara.app_host = "http://localhost:3000"
   @session ||= Capybara::Session.new(:Akephalos)
@@ -5,8 +7,11 @@ end
 alias page session
 
 module Akephalos
+  # Simple class for starting an IRB session.
   class Console
 
+    # Start an IRB session. Tries to load irb/completion, and also loads a
+    # .irbrc file if it exists.
     def self.start
       require 'irb'
 
